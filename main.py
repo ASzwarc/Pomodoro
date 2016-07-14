054c4fc8aed20eef3de4bdd0eaacfdc63ac6badd
Szwarc Adam <adam.szwarc@nokia.com>
2016-07-14 22:39:14 +0200

    Main loop finished. Each timer started in new thread - its not needed.

--- main.py
+++ main.py
@@ -4,26 +4,48 @@ import winsound
 import logging
 
 logging.basicConfig(level=logging.DEBUG, format='[%(levelname)s] (%(threadName)-10s) %(message)s')
-work_time = 0.2
-short_break = 0.1
-long_break = 0.3
-pomodoro_units_counter = 0
+work_time = 0.1
+short_break = 0.05
+long_break = 0.2
+unit_size = 4
 
 def run_timer(length_type):
-    if length_type == 0:
-        elapsed_time = Timer(work_time * 60, on_timeout)
-    elif length_type == 1:
-        elapsed_time = Timer(short_break * 60, on_timeout)
-    elif length_type == 2:
-        elapsed_time = Timer(long_break * 60, on_timeout)
+    if length_type == 'work':
+        elapsed_time = Timer(work_time * 60, program_control, args=['work'])
+    elif length_type == 'short_break':
+        elapsed_time = Timer(short_break * 60, program_control, args=['short_break'])
+    elif length_type == 'long_break':
+        elapsed_time = Timer(long_break * 60, program_control, args=['long_break'])
     else:
         raise ValueError("Allowed types of timers are in range [0;2]!")
     elapsed_time.start()
 
-def on_timeout():
-    logging.info("Timer stopped")
-    winsound.Beep(300, 1200)
-    user_interface()
+def program_control(step):
+    global pomodoro_units_counter
+    if step == 'start':
+        pomodoro_units_counter = 0
+        logging.debug("Starting sequence. Step = " + step + ", units = " + str(pomodoro_units_counter))
+        run_timer('work')
+    elif step == 'work' and pomodoro_units_counter < unit_size:
+        winsound.Beep(300, 1200)
+        pomodoro_units_counter +=1
+        logging.debug("Small unit finished. Step = " + step + ", units = " + str(pomodoro_units_counter))
+        run_timer('short_break')
+    elif step == 'work' and pomodoro_units_counter >= unit_size:
+        winsound.Beep(300, 1200)
+        pomodoro_units_counter = 0
+        logging.debug("Big unit finished. Step = " + step + ", units = " + str(pomodoro_units_counter))
+        run_timer('long_break')
+    elif step == 'short_break':
+        winsound.Beep(300, 1200)
+        logging.debug("Short break finished. Step = " + step + ", units = " + str(pomodoro_units_counter))
+        run_timer('work')
+    elif step == 'long_break':
+        winsound.Beep(300, 1200)
+        logging.info("Pomodoro big unit finished!")
+        user_interface()
+    else:
+        raise ValueError("Undefined step value!")
 
 def user_interface():
     logging.info("Action:")
@@ -34,7 +56,7 @@ def user_interface():
 
     if str(chosen_option) == "1":
         logging.debug("starting timer")
-        run_timer(0)
+        program_control('start')
     elif str(chosen_option) == "2":
         logging.debug("option 2")
         # Threads have to be saved in some structure to enable its canceling
