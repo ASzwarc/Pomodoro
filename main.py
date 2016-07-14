226406b104953a5770ffdfa73c1211653b6003c8
Szwarc Adam <adam.szwarc@nokia.com>
2016-07-14 18:09:29 +0200

    Added user interface and modified main loop. Program works has bottlenecks and works like single threaded

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 #!usr/bin/python
 from threading import Timer
 import winsound
+import logging
 
+logging.basicConfig(level=logging.DEBUG, format='[%(levelname)s] (%(threadName)-10s) %(message)s')
 work_time = 0.2
 short_break = 0.1
 long_break = 0.3
@@ -19,24 +21,30 @@ def run_timer(length_type):
     elapsed_time.start()
 
 def on_timeout():
-    print("Timer stopped")
+    logging.info("Timer stopped")
     winsound.Beep(300, 1200)
+    user_interface()
+
+def user_interface():
+    logging.info("Action:")
+    logging.info("1 - Start working")
+    logging.info("2 - Cancel timers and exit program")
+    logging.info("What's your decision: ")
+    chosen_option = input()
+
+    if str(chosen_option) == "1":
+        logging.debug("starting timer")
+        run_timer(0)
+    elif str(chosen_option) == "2":
+        logging.debug("option 2")
+        # Threads have to be saved in some structure to enable its canceling
+        logging.info("Goodbye")
+    else:
+        logging.debug("option 3")
+        logging.info("Goodbye")
 
 def main():
-    while True:
-        print("Action:")
-        print("1 - Start working")
-        print("2 - Cancel timers and exit program")
-        chosen_option = input("What's your decision: ")
-
-        if chosen_option == "1":
-            run_timer(0)
-        elif chosen_option == "2":
-            #Threads have to be saved in some structure to enable its canceling
-            break
-        else:
-            break
-    print("Goodbye")
+    user_interface()
 
 if __name__ == '__main__':
     main()
