5dd9afce1b46a02772b7291cb2577c7cfd1481d2
AdamSzwarc <szwarc.adam@gmail.com>
2016-09-20 16:40:13 +0200

    Changed names of widgets in design. Added empty handlers for button actions

--- controller.py
+++ controller.py
@@ -1,7 +1,19 @@
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtCore
 import design
 
 class Controller(QtWidgets.QMainWindow, design.Ui_MainWindow):
     def __init__(self):
         super(self.__class__, self).__init__()
         self.setupUi(self)
+        self.startButton.clicked.connect(self.start_timer)
+        self.stopButton.clicked.connect(self.stop_timer)
+        self.resetButton.clicked.connect(self.reset_timer)
+    
+    def start_timer(self):
+        pass
+    
+    def stop_timer(self):
+        pass
+    
+    def reset_timer(self):
+        pass
