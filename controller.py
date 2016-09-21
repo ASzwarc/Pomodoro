816bdf099384e82bbe250e77e7329c9560776530
AdamSzwarc <szwarc.adam@gmail.com>
2016-09-21 14:03:04 +0200

    Plain timer functionality added

--- controller.py
+++ controller.py
@@ -1,19 +1,34 @@
 from PyQt5 import QtWidgets, QtCore
 import design
+import time
 
 class Controller(QtWidgets.QMainWindow, design.Ui_MainWindow):
     def __init__(self):
         super(self.__class__, self).__init__()
+        self.initialize_ui()
+        self.timer = QtCore.QTimer(self)
+        self.timer.timeout.connect(self.handle_timeout)
+        self.start_time = time.time()
+    
+    def initialize_ui(self):
         self.setupUi(self)
-        self.startButton.clicked.connect(self.start_timer)
-        self.stopButton.clicked.connect(self.stop_timer)
-        self.resetButton.clicked.connect(self.reset_timer)
+        self.startButton.clicked.connect(self.handle_start_button)
+        self.stopButton.clicked.connect(self.handle_stop_button)
+        self.resetButton.clicked.connect(self.handle_reset_button)
+        self.elapsedTimeLabel.setText("0:00:00")
+
+    def handle_start_button(self):
+        self.start_time = time.time()
+        self.timer.start(1000)
+
+    def handle_stop_button(self):
+        self.timer.stop()
     
-    def start_timer(self):
-        pass
-    
-    def stop_timer(self):
-        pass
-    
-    def reset_timer(self):
-        pass
+    def handle_reset_button(self):
+        self.timer.stop()
+        self.elapsedTimeLabel.setText("0:00:00")
+
+    def handle_timeout(self):
+        elapsed_time = str(time.time() - self.start_time)
+        self.elapsedTimeLabel.setText(elapsed_time)
+        self.timer.start()
